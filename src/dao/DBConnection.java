5098f1886270790ca2a7c8df90850afe316620b8
Nithiya-B <nithiyabaskaran1103@gmail.com>
2025-12-25 15:43:48 +0530

    changed

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
             return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/roomease",
                 "root",
-                "password"   // change password
+                "Nithu_1103"   // change password
             );
         } catch (Exception e) {
             e.printStackTrace();
